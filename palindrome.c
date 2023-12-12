0f4097e92f0f60ff0da4086da63ac5fe63c9fca3
NIKHILUTTAM <153156217+NIKHILUTTAM@users.noreply.github.com>
2023-12-12 17:57:22 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int n,x,r,sum=0;
+    printf("enter the no.:");
+    scanf("%d",&n);
+    x=n;
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum*10+r;
+        n=n/10;
+    }
+    if(x==sum)
+     printf("no. is palindrome");
+    else
+     printf("not a palindrome");
+}
